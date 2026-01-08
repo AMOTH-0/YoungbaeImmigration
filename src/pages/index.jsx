666e6081fdeda562d1ed0fb2d751edea3c93cedd
unknown <djc07202@gmail.com>
2026-01-07 21:34:55 -0600

    Fix routing issues: correct route paths, set Home as default route, clean up unused code

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,53 +8,18 @@ import Home from "./Home";
 
 import Services from "./Services";
 
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
-const PAGES = {
-    
-    About: About,
-    
-    Contact: Contact,
-    
-    Home: Home,
-    
-    Services: Services,
-    
-}
-
-function _getCurrentPage(url) {
-    if (url.endsWith('/')) {
-        url = url.slice(0, -1);
-    }
-    let urlLastPart = url.split('/').pop();
-    if (urlLastPart.includes('?')) {
-        urlLastPart = urlLastPart.split('?')[0];
-    }
-
-    const pageName = Object.keys(PAGES).find(page => page.toLowerCase() === urlLastPart.toLowerCase());
-    return pageName || Object.keys(PAGES)[0];
-}
-
-// PagesContent component that uses useLocation (Router context provided by HashRouter in main.jsx)
+// Pages component - Router context provided by HashRouter in main.jsx
 export default function Pages() {
-    const location = useLocation();
-    const currentPage = _getCurrentPage(location.pathname);
-    
     return (
-        <Layout currentPageName={currentPage}>
+        <Layout>
             <Routes>            
-                
-                    <Route path="/" element={<About />} />
-                
-                
-                <Route path="/About" element={<About />} />
-                
-                <Route path="/Contact" element={<Contact />} />
-                
-                <Route path="/Home" element={<Home />} />
-                
-                <Route path="/Services" element={<Services />} />
-                
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/contact" element={<Contact />} />
             </Routes>
         </Layout>
     );
